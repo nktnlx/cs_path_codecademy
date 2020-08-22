f206960bd83f28925288d088392f4256a647b73e
nktnlx <nktn.lx@gmail.com>
2020-08-22 17:08:59 +0300

    with digits

--- 6_censor_engine/censor_engine.py
+++ 6_censor_engine/censor_engine.py
@@ -13,13 +13,13 @@ parser.add_argument('--filename', help='name of a txt file to censor words/digit
 args = parser.parse_args()
 
 
-# opening and reading a text to censor
+# opening and reading a text file to censor
 with open(args.filename) as txt_file:
-    input_txt = txt_file.read()
+    input_text = txt_file.read()
 
 
 # making a list with punctuation signs to use it later
-# when checkin the proper delimeter
+# when checkin the proper delimeter (in user input)
 punctuation = []
 [punctuation.append(string.punctuation[i]) for i in range(len(string.punctuation))]
 punctuation.append(' ')
@@ -54,17 +54,9 @@ Words should be comma separated (e.g. fact, check, brother): ''').lower()
     return user_list, user_sign
 
 
-#digits = string.digits
-#print(digits)
-
-#print(input_txt)
-#print(input_txt.replace('cat', '*'*len('cat')))
-#txt_as_lst = input_txt.split()
-#print(txt_as_lst)
-
-
-# half of terminal screen width in Ubuntu for proper center alignment
+# half of terminal screen width in Ubuntu for proper center alignment of output
 half_width_terminal = 44
+
 
 # greeting user
 def greet():
@@ -92,6 +84,38 @@ def print_censor_words(to_censor, sign):
     print('\n')
 
 
+# printing digits to censor
+def print_digits(to_censor, sign):
+    print('\n')
+    print('-' * (half_width_terminal * 2))
+    to_censor_string = ', '.join(to_censor)
+    print(f'ALL DIGITS WILL BE CENSORED IN THE TEXT: {to_censor_string}')
+    print(f'USER DEFINED SIGN TO BE USED FOR CENSORING: {sign}')
+    print('-' * (half_width_terminal * 2))
+    print('\n')
+
+
+# censoring text
+def censor_engine(text, search_list, repl):
+    alphabet = string.ascii_letters
+    for word in search_list:
+        index = 0
+        count = 0
+        text_lower = text.lower()
+        while index < len(text):
+            index = text_lower.find(word) + count
+            if index < 0:
+                break
+            if (text[index:index + len(word)]).lower() == word and text[index + len(word):index + len(word) + 1] not in alphabet:
+                text_lower = text_lower[0:index] + text_lower[index:index+len(word)].replace(word, repl * len(word)) + text_lower[index+len(word):]
+                text = text[0:index] + text[index:index+len(word)].replace(text[index:index+len(word)], repl * len(word)) + text[index+len(word):]
+                count = 0
+            else:
+                count += 1
+
+    return text
+
+
 # printing censored text
 def censored_txt(text):
     print('\n' + 'the censored text:'.upper())
@@ -99,13 +123,33 @@ def censored_txt(text):
     print(text)
 
 
+# the program runs one of scenarios based on the user choice: substituting words or digits
 def run_the_script():
-    user_words, user_sign = user_input()
-    user_words_plurals = plurals(user_words)
-    original_txt(input_txt)
-    print_censor_words(user_words, user_sign)
-    censored_txt(input_txt)
-    exit()
+    user_choice = None
+    # looping to receive a correct user input (either of two available options)
+    while not (user_choice == '1' or user_choice == '0'):
+        user_choice = input('''What you would like to censor?
+Press: 1 - for 'USER SPECIFIED WORDS', 0 - for 'ALL DIGITS': ''')
+
+        # scenario for substituting user defined words in the text
+        if user_choice == '1':
+            user_words, user_sign = user_input()
+            user_words_plurals = plurals(user_words)
+            original_txt(input_text)
+            print_censor_words(user_words, user_sign)
+            output_text = censor_engine(input_text, user_words_plurals, user_sign)
+            censored_txt(output_text)
+            exit()
+
+        # scenario for substituting all digits in the text
+        if user_choice == '0':
+            user_words = [str(i) for i in range(10)]
+            user_sign = input('Please, enter a single sign you\'d like to be used for censoring: ')
+            original_txt(input_text)
+            print_digits(user_words, user_sign)
+            output_text = censor_engine(input_text, user_words, user_sign)
+            censored_txt(output_text)
+            exit()
 
 
 greet()
